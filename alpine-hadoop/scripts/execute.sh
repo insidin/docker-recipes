ed50efcb6b875427893da24a65a5bcdba3aa32db
Jan <jan@xorto.be>
2015-10-06 15:59:39 +0200

    added hook to run install script located on mapped host folder

--- alpine-hadoop/scripts/execute.sh
+++ alpine-hadoop/scripts/execute.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 su -c "chown -R $USER:$GROUP $DATA_MAPPING_DIR"
+
+if [ -f "${HOST_MAPPING_DIR}/install/install.sh" ]; then
+  . "${HOST_MAPPING_DIR}/install/install.sh"
+fi
+
 su -s /bin/bash -c "$@" $USER
